df31fc060dbe0a487057881d10ab21e51209d9c2
risbudaditya <risbud.aditya@gmail.com>
2016-04-24 11:58:41 -0400

    Added files via upload
    Added in up/down support. Exercise type will be different for the two pair and provide a different amount of thrust to the two.

--- ISSIE-unity/Assets/Scripts/WebSocketController.cs
+++ ISSIE-unity/Assets/Scripts/WebSocketController.cs
@@ -23,22 +23,36 @@ public class WebSocketController : MonoBehaviour, WebSocketUnityDelegate {
 	// Use this for initialization
 	void Start () {
 	}
-	
-	public int getHumanPower() {
-		int force = 0;
+
+    public enum DIRECTION { UP, DOWN, LEFT, RIGHT, NONE, };
+    public class PowerSignal
+    {
+        private int m_force;
+        private DIRECTION m_direction;
+        public PowerSignal(int force, DIRECTION direction)
+        {
+            m_force = force;
+            m_direction = direction;
+        }
+        public int getForce() { return m_force; }
+        public DIRECTION getDirection() { return m_direction; }
+    }
+
+	public PowerSignal getHumanPower() {
+        PowerSignal signal = new PowerSignal(0, DIRECTION.NONE);
 
 		if (_message != "") {
-			if (_message == "right") {
-				force = 10;
-			} else {
-				force = -10;
-			}
-
+            switch( _message)
+            {
+                case "right": signal = new PowerSignal(1, DIRECTION.RIGHT); break;
+                case "left": signal = new PowerSignal(-1, DIRECTION.LEFT); break;
+                case "up": signal = new PowerSignal(1, DIRECTION.UP); break;
+                case "down": signal = new PowerSignal(-1, DIRECTION.DOWN); break;
+                default: break;
+            }
 			_message = "";
-
 		}
-
-		return force;
+		return signal;
 	}
 
 
